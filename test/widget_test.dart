07701a343910c0481d6809a3782270c5705a729a
Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>
2025-03-12 00:47:12 +0100

    Merging
    Signed-off-by: Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ia_storyteller/main.dart';
+import 'package:ia_storyteller/selectionscreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
